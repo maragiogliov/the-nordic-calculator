033aac617a987d45c15a542841c8c0e39c1734f8
Victor <maragiogliov@protonmail.com>
2023-01-17 15:32:03 +0100

    documentation for icons navigation

--- src/content/IconsNavigation/IconsNavigation.js
+++ src/content/IconsNavigation/IconsNavigation.js
@@ -3,14 +3,27 @@ import { Button } from '@carbon/react';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router-dom'
 
-
-import {Home} from '@carbon/react/icons';
+import { Home } from '@carbon/react/icons';
 import { PlanePrivate } from '@carbon/react/icons';
-import {Car} from '@carbon/react/icons';
-import {Scooter} from '@carbon/react/icons';
-import {Bus} from '@carbon/react/icons';
-import {Add} from '@carbon/react/icons';
+import { Car } from '@carbon/react/icons';
+import { Scooter } from '@carbon/react/icons';
+import { Bus } from '@carbon/react/icons';
+import { Add } from '@carbon/react/icons';
 import { ChartLineData } from '@carbon/react/icons';
+
+
+    {/* -------------------------------
+  Imported the withRouter Higher-Order Component (HOC) from the react-router-dom library to get access to the current location object.
+
+  Wrapped the IconsNavigation component with the withRouter HOC to gain access to the location prop.
+
+  Added a class of 'active' to the Link element surrounding each icon when the current pathname matches the route of the icon.
+
+  Used the class of 'active' to change the color of the icon to blue when the icon's corresponding route is active.
+
+  Overall, these changes allow the icons to visually indicate which route the user is currently on and provides a clear way to navigate between different routes using icons.
+  
+  ------------------------------------ */}
 
 
 
